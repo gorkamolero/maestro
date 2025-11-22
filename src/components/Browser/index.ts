09caee1d7d119fb2a40832c8e79103c510b4556b
gorkamolero <gorka.molero@gmail.com>
2025-11-22 09:26:12 +0100

    Clean up Browser exports

--- src/components/Browser/index.ts
+++ src/components/Browser/index.ts
@@ -3,10 +3,3 @@
  */
 
 export { BrowserPanel } from './BrowserPanel';
-export { BrowserTab } from './BrowserTab';
-export { TabBar } from './TabBar';
-export { NavigationBar } from './NavigationBar';
-export { BrowserContent } from './BrowserContent';
-export * from './browser.utils';
-export { useWebview } from './useWebview';
-export { useNavigation } from './useNavigation';
